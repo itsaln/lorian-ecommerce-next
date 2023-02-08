e8bd5d4bd43fc2b080db4b9e891bb10e43668728
itsaln <itsaln23@gmail.com>
2023-02-08 17:00:50 +0500

    init

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import { GetStaticProps, NextPage } from 'next'
+
+import Home from '@/screens/home/Home'
+
+import { ProductService } from '@/services/ProductService'
+
+import { IProduct } from '@/types/product.interface'
+
+export interface IProductsPage {
+	products: IProduct[]
+}
+
+const HomePage: NextPage<IProductsPage> = ({ products }) => {
+	return <Home products={products} />
+}
+/* 
+export const getStaticProps: GetStaticProps<IProductsPage> = async () => {
+	const products = await ProductService.getProducts()
+
+	return {
+		props: {
+			products
+		}
+	}
+} */
+
+export default HomePage
