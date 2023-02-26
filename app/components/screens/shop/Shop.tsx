4c2b730616b82005fccd0d8eb796aa0c3ba79457
itsaln <itsaln23@gmail.com>
2023-02-26 12:06:19 +0500

    01

--- app/components/screens/shop/Shop.tsx
+++ app/components/screens/shop/Shop.tsx
@@ -10,8 +10,11 @@ import Column from '@/ui/grid/Column'
 import Row from '@/ui/grid/Row'
 
 import { IProductsPage } from '../../../../pages'
+import { formatToCurrency } from '@/utils/format-to-currency'
 
 const Shop: FC<IProductsPage> = ({ products }) => {
+	console.log('products:---', products)
+
 	return (
 		<Layout title='Shop' description='Catalog clothing and accessories'>
 			<Row
@@ -21,21 +24,27 @@ const Shop: FC<IProductsPage> = ({ products }) => {
 					backgroundImage: 'url(/images/texture.jpg)'
 				}}
 			>
-				{products.map((product, index) => (
-					<Column key={product.id} size={index === 2 ? 3 : index === 4 ? 3 : 2}>
-						<div>
-							<Image
-								src={product.images[0]}
-								alt={product.name}
-								width={220}
-								height={220}
-							/>
-							<h2>{product.name}</h2>
-							<div className=''>{product.description}</div>
-							<button>Add to cart</button>
-						</div>
-					</Column>
-				))}
+				{/*{!!products.length &&*/}
+				{/*	products.map((product, index) => (*/}
+				{/*		<Column*/}
+				{/*			key={product.id}*/}
+				{/*			size={index === 2 ? 3 : index === 4 ? 3 : 2}*/}
+				{/*		>*/}
+				{/*			<div className='text-white text-center pb-5'>*/}
+				{/*				<div className='flex items-center justify-center mb-4' style={{height: 283}}>*/}
+				{/*					<Image*/}
+				{/*						src={product.images[0]}*/}
+				{/*						alt={product.name}*/}
+				{/*						width={220}*/}
+				{/*						height={220}*/}
+				{/*					/>*/}
+				{/*				</div>*/}
+				{/*				<h2 className='font-serif text-lg mb-2'>{product.name}</h2>*/}
+				{/*				<div className='mb-3'>{formatToCurrency(product.price)}</div>*/}
+				{/*				<button className='btn-link'>Add to cart</button>*/}
+				{/*			</div>*/}
+				{/*		</Column>*/}
+				{/*	))}*/}
 				<Column
 					size={5}
 					isPadding={false}
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,15 +1,23 @@
 import { GetStaticProps, NextPage } from 'next'
 
 import Shop from '@/screens/shop/Shop'
-import { IProductsPage } from './index'
+
 import { ProductService } from '@/services/product.service'
 
-const ShopPage: NextPage = () => {
-	return <Shop />
+import { IProductsPage } from './index'
+
+const ShopPage: NextPage<IProductsPage> = ({ products }) => {
+	return <Shop products={products} />
 }
 
 export const getStaticPops: GetStaticProps<IProductsPage> = async () => {
 	const products = await ProductService.getProducts()
+
+	return {
+		props: {
+			products
+		}
+	}
 }
 
 export default ShopPage
